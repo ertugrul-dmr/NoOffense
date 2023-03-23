bf2d6118921dd78173dc80270f1859ab1c88125c
ertugrul-dmr <jucshin@gmail.com>
2023-03-23 15:38:21 +0300

    initial releasev2

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='toolbox',
     version='0.0.1',
-    author='ert',
+    author='ertugrul',
     author_email='',
     description='Testing installation of Package',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
     url='https://github.com/ertugrul-dmr/NoOffense',
     project_urls = {
         "Bug Tracker": "https://github.com/ertugrul-dmr/NoOffense/issues"
     },
     license='Apache',
-    packages=['noffense'],
-    install_requires=['string', 're'],
+    packages=['nooffense'],
+    #install_requires=['string', 're'],
 )
