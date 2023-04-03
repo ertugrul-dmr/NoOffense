3238c5191fc0096aba40377307aa865d24262ff5
ertugrul-dmr <jucshin@gmail.com>
2023-04-03 13:43:33 +0300

    updated setup configuration

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+with open("README.md", mode="r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
 
 setuptools.setup(
     name='no-offense',
@@ -9,10 +10,23 @@ setuptools.setup(
     author='ertugrul',
     author_email='',
     description='A smart and fast offensive language detection tool for Turkish',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url='https://github.com/ertugrul-dmr/NoOffense',
     project_urls = {
         "Bug Tracker": "https://github.com/ertugrul-dmr/NoOffense/issues"
     },
-    license='Apache',
-    packages=['nooffense']
+    license="Apache License 2.0",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6.0",
+    install_requires=[
+        "torch>=2.0.0"
+        "transformers>=4.6.0,<5.0.0"
+        "pandas"
+        "tqdm"
+        "numpy"
+        "sentencepiece",
+        "huggingface-hub>=0.4.0"
+
+    ],
 )
